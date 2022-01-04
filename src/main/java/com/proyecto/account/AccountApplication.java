cd6aa63158cf76bfa1b933fb2f95c5742381bb27
Choquidownn25 <jose_sarria2003@yahoo.com.mx>
2022-01-03 22:45:35 -0500

    Text File
    Crear Texto para log de navegacion

--- src/main/java/com/proyecto/account/AccountApplication.java
+++ src/main/java/com/proyecto/account/AccountApplication.java
@@ -1,5 +1,6 @@
 package com.proyecto.account;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.proyecto.account.repository.StorageService;
 import com.proyecto.account.util.StorageProperties;
 import org.springframework.boot.CommandLineRunner;
@@ -10,9 +11,13 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 @EnableConfigurationProperties(StorageProperties.class)
 public class AccountApplication {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(AccountApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(AccountApplication.class, args);
+		LOGGER.error("Message logged at ERROR level");
+		LOGGER.warn("Message logged at WARN level");
+		LOGGER.info("Message logged at INFO level");
+		LOGGER.debug("Message logged at DEBUG level");
 	}
 	@Bean
 	CommandLineRunner init(StorageService storageService) {
